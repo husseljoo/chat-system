d0df6d2da2e60d0c93a2d12d68db342ad94fc76c
Husseljo <husseljoo@gmail.com>
2024-05-10 18:00:01 +0300

    added set token in redis for create_application endpoint

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,3 +1,6 @@
+require "net/http"
+require "securerandom"
+
 class ApplicationsController < ApplicationController
   before_action :set_application, only: %i[ show update destroy ]
 
@@ -13,12 +16,28 @@ class ApplicationsController < ApplicationController
     render json: @application
   end
 
+  def generate_token(length = 12)
+    SecureRandom.alphanumeric(length)
+  end
+
   # POST /applications
   def create
-    @application = Application.new(application_params)
+    token = generate_token
+
+    url = "http://localhost:8081/chat?app_token=#{token}"
+    puts "URL:  #{url}"
+    res = set_token_redis(url)
+    puts "Res:  #{res}"
+    if res == false
+      render json: { error: "Failed to set token: #{token}" }, status: :unprocessable_entity
+      return
+    end
+
+    @application = Application.new(application_params.merge(token: token))
 
     if @application.save
-      render json: @application, status: :created, location: @application
+      # render json: @application, status: :created, location: @application
+      render json: { token: token }, status: :created, location: @application
     else
       render json: @application.errors, status: :unprocessable_entity
     end
@@ -39,13 +58,37 @@ class ApplicationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_application
-      @application = Application.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def application_params
-      params.fetch(:application, {})
+  # Use callbacks to share common setup or constraints between actions.
+  def set_application
+    @application = Application.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  # def application_params
+  #   params.fetch(:application, {})
+  # end
+
+  def application_params
+    params.permit(:name)
+  end
+
+  def set_token_redis(url)
+    uri = URI(url)
+    request = Net::HTTP::Put.new(uri)
+
+    begin
+      response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+        http.request(request)
+      end
+
+      if response.is_a?(Net::HTTPSuccess)
+        return true
+      end
+      return false
+    rescue StandardError => e
+      puts "Error: #{e.message}"
+      return nil
     end
+  end
 end
