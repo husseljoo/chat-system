fdfb29ebd010ebc7dcfb393eb3f3b5d56a9ad1e2
Husseljo <husseljoo@gmail.com>
2024-05-12 00:47:47 +0300

    change name to REDIS_URL

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,11 +2,11 @@
 
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: ENV.fetch("SIDEKIQ_REDIS_URL", "redis://localhost:6379/2"),
+    url: ENV.fetch("REDIS_URL", "redis://localhost:6379/2"),
   }
 end
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: ENV.fetch("SIDEKIQ_REDIS_URL", "redis://localhost:6379/2"),
+    url: ENV.fetch("REDIS_URL", "redis://localhost:6379/2"),
   }
 end
